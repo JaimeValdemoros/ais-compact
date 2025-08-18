d265a4b2293753cb42f0c1bba0cf42d9535db63b
Jaime Valdemoros <JaimeValdemoros@users.noreply.github.com>
2025-08-18 00:26:53 +0000

    Verify checksums before and after sending

--- src/bin/decompress.rs
+++ src/bin/decompress.rs
@@ -4,8 +4,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut stdin = std::io::stdin().lock();
     let mut reader = protobuf::CodedInputStream::from_buf_read(&mut stdin);
     let mut stdout = std::io::stdout().lock();
+
+    // Buffer to avoid repeated allocations
+    let mut buf = Vec::new();
+
     while !reader.eof()? {
         let message = reader.read_message::<ais_compact::proto::spec::Message>()?;
+
+        if message.has_encoded() {
+            buf.clear();
+            message.try_write(&mut buf)?;
+            let s = std::str::from_utf8(&buf)?;
+            if !ais_compact::sentence::verify_checksum(s)? {
+                return Err(anyhow::anyhow!("Invalid checksum").into());
+            }
+        }
+
         message.try_write(&mut stdout)?;
         stdout.write_all(b"\n")?;
     }
--- src/sentence.rs
+++ src/sentence.rs
@@ -3,6 +3,38 @@ use std::fmt;
 use bit_struct::*;
 use either::Either;
 
+pub fn verify_checksum(s: &str) -> anyhow::Result<bool> {
+    use winnow::Parser;
+    use winnow::error::StrContext;
+    use winnow::token::{take, take_until};
+
+    // helper function to parse out key segments
+    fn parse_inner<'a>(s: &mut &'a str) -> winnow::Result<(&'a str, u8)> {
+        '!'.parse_next(s)?;
+        let main = take_until(1.., '*')
+            .context(StrContext::Label("main"))
+            .parse_next(s)?;
+        '*'.parse_next(s)?;
+        let checksum = take(2usize)
+            .try_map(|s| u8::from_str_radix(s, 16))
+            .context(StrContext::Label("checksum"))
+            .parse_next(s)?;
+        Ok((main, checksum))
+    }
+
+    let (main, checksum) = {
+        let mut s = s;
+        parse_inner
+            .parse(&mut s)
+            .map_err(|e| anyhow::format_err!("\n{e}"))?
+    };
+    let mut acc = checksum;
+    for char in main.chars() {
+        acc ^= u8::try_from(char)?;
+    }
+    Ok(acc == 0)
+}
+
 enums! {
     pub TalkerID { AB, AD, AI, AN, AR, AS, AT, AX, BS, SA }
 
